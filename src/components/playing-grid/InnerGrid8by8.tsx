7b2fba5ac109daf8b5c23eaf9487ca7e82f51097
Ruhul <itsruhul@proton.me>
2026-03-14 23:00:29 +0530

    remove num clicked on grid from random16set

--- src/components/playing-grid/InnerGrid8by8.tsx
+++ src/components/playing-grid/InnerGrid8by8.tsx
@@ -2,15 +2,17 @@ import {useState} from 'react';
 
 export function InnerGrid8by8() {
 
-  //const gridArray = [];
+  const [clickedNum, setClickedNum] = useState(0);
 
   //Creating a set containing 16 random numbers <= 64.
   const [random16] = useState<Set<number>>(() => {
     const random16Set = new Set<number>();
 
     while(random16Set.size < 16) {
-      const randomNo = Math.floor(Math.random() * 64);
-      random16Set.add(randomNo);
+      if (!clickedNum) {
+        const randomNo = Math.floor(Math.random() * 64);
+        random16Set.add(randomNo);
+      }
     }
     return random16Set;
   });
